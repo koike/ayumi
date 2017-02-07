999a6eb4343a1afcf0281e616caad64897e1889e
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-07 17:46:48 +0900

    :up: Updated ayumi.php

--- ayumi.php
+++ ayumi.php
@@ -28,60 +28,63 @@ register_shutdown_function('shutdown_handler');
 $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
 
-// csvを取得する
-$url = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip';
-$data = file_get_contents($url);
-file_put_contents('top-1m.csv.zip', $data);
-
-$zip = new ZipArchive;
-if ($zip->open('top-1m.csv.zip') === true)
+while(true)
 {
-    $zip->extractTo(__DIR__);
-    $zip->close();
-}
-unlink('top-1m.csv.zip');
+    // csvを取得する
+    $url = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip';
+    $data = file_get_contents($url);
+    file_put_contents('top-1m.csv.zip', $data);
 
-// csvを読み込む
-$file = new SplFileObject('top-1m.csv');
-$file->setFlags(SplFileObject::READ_CSV);
-$urls = [];
-foreach ($file as $line)
-{
-    if(count($line) == 2)
+    $zip = new ZipArchive;
+    if ($zip->open('top-1m.csv.zip') === true)
     {
-        $url = 'http://' . $line[1];
-        array_push($urls, $url);
+        $zip->extractTo(__DIR__);
+        $zip->close();
     }
-}
+    unlink('top-1m.csv.zip');
 
-$urls = array_reverse($urls);
-$c = count($urls);
-for($i=0; $i<$c; $i++)
-{
-    $url = $urls[$i];
-
-    $date = date('Y-m-d H:i:s');
-    DB::table('URL')
-    ->insert
-    (
-        [
-            'url'           =>  $url,
-            'created_at'    =>  $date
-        ]
-    );
-
-    $response = Request::get($url);
-    $ayumi = new Analyze($url);
-    $ayumi->analyze($response);
-    
-    if($ayumi->get_is_mallicious())
+    // csvを読み込む
+    $file = new SplFileObject('top-1m.csv');
+    $file->setFlags(SplFileObject::READ_CSV);
+    $urls = [];
+    foreach ($file as $line)
     {
-        $ayumi->register_db();
-        Notificate::slack($ayumi);
-        echo '[*] (' . ($c-$i) . ') ' . $url . PHP_EOL;
+        if(count($line) == 2)
+        {
+            $url = 'http://' . $line[1];
+            array_push($urls, $url);
+        }
     }
-    else
+
+    $urls = array_reverse($urls);
+    $c = count($urls);
+    for($i=0; $i<$c; $i++)
     {
-        echo '[-] (' . ($c-$i) . ') ' . $url . PHP_EOL;
+        $url = $urls[$i];
+
+        $date = date('Y-m-d H:i:s');
+        DB::table('URL')
+        ->insert
+        (
+            [
+                'url'           =>  $url,
+                'created_at'    =>  $date
+            ]
+        );
+
+        $response = Request::get($url);
+        $ayumi = new Analyze($url);
+        $ayumi->analyze($response);
+        
+        if($ayumi->get_is_mallicious())
+        {
+            $ayumi->register_db();
+            Notificate::slack($ayumi);
+            echo '[*] (' . ($c-$i) . ') ' . $url . PHP_EOL;
+        }
+        else
+        {
+            echo '[-] (' . ($c-$i) . ') ' . $url . PHP_EOL;
+        }
     }
 }
