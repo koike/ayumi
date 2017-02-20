9afda3df26cb02c207dd16831b8ae5e18e67fcbf
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-20 11:30:57 +0900

    :up: Updated files

--- lib/loader.php
+++ lib/loader.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once 'vendor/autoload.php';
+require_once 'phirehose/Phirehose.php';
+require_once 'phirehose/OauthPhirehose.php';
 
 // require_once /lib/*.php
 foreach(glob(__DIR__ . '/*.php') as $file)
@@ -13,6 +15,15 @@ foreach(glob(__DIR__ . '/*.php') as $file)
 
 // require_once /lib/Campaign/*.php
 foreach(glob(__DIR__ . '/Campaign/*.php') as $file)
+{
+    if(is_file($file))
+    {
+        require_once $file;
+    }
+}
+
+// require_once /lib/EK/*.php
+foreach(glob(__DIR__ . '/EK/*.php') as $file)
 {
     if(is_file($file))
     {
