b645df9bc3c9ac08349cb7f4078a45c6d5157957
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-03-03 10:53:26 +0900

    :new: Add lib/Campaign/Nebula.php

---
+++ lib/Campaign/Nebula.php
@@ -0,0 +1,18 @@
+<?php
+
+class Nebula
+{
+    public static function analyze(string $html) : bool
+    {
+        // URLパラメータの値が56文字で, base64文字列
+        if(preg_match('/\/\?[0-9a-zA-Z_\-]{1,30}=[0-9a-zA-Z_\-]{56}/', $html, $result))
+        {
+            $base64_str = explode('=', $result[0])[1];
+            if(base64_decode($base64_str))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
