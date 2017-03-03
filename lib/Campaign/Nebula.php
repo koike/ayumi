7888b3297acae0126997ab8ed7254860ff126c6b
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-03-03 12:36:56 +0900

    :up: Updated lib/Campaign/Nebula.php

--- lib/Campaign/Nebula.php
+++ lib/Campaign/Nebula.php
@@ -4,8 +4,13 @@ class Nebula
 {
     public static function analyze(string $html) : bool
     {
+        $html = str_replace("\r", "", $html);
+        $html = str_replace("\n", "", $html);
+        $html = str_replace("\t", "", $html);
+        $html = str_replace(' ', '', $html);
+
         // <div style='width: 1px; height: 1px; position: absolute; left:-500px; top: -500px;'>
-        if(preg_match('/<div style=\'width: 1px; height: 1px; position: absolute; left:-500px; top: -500px;\'>/', $html, $result))
+        if(preg_match('/<divstyle=\'width:1px;height:1px;position:absolute;left:-500px;top:-500px;\'>/', $html, $result))
         {
             return true;
         }
