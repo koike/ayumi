da47080170f1ce5fec447d42fdf72076c077cb4a
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-03-03 11:31:07 +0900

    :up: Updated lib/Campaign/Nebula.php

--- lib/Campaign/Nebula.php
+++ lib/Campaign/Nebula.php
@@ -5,9 +5,11 @@ class Nebula
     public static function analyze(string $html) : bool
     {
         // URLパラメータの値が56文字で, base64文字列
-        if(preg_match('/\/\?[0-9a-zA-Z_\-]{1,30}=[0-9a-zA-Z_\-]{56}/', $html, $result))
+        if(preg_match('/\/\?[0-9a-zA-Z_\-]{1,30}=[0-9a-zA-Z_\-]{56}["|\']/', $html, $result))
         {
             $base64_str = explode('=', $result[0])[1];
+            $base64_str = str_replace('"', '', $base64_str);
+            $base64_str = str_replace("'", '', $base64_str);
             if(base64_decode($base64_str))
             {
                 return true;
