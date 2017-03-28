d74e16c593d34ef29707ccb61110582416440733
Kirin MORINO <info@o8o.jp>
2017-03-28 11:37:41 +0900

    :up: Updated lib/Campaign/HookAds.php

--- lib/Campaign/HookAds.php
+++ lib/Campaign/HookAds.php
@@ -5,7 +5,7 @@ class HookAds
     public static function analyze(string $html) : bool
     {
         // <center><iframe src='URL' frameborder='0' scrolling='no' width='xxx' height='xx'></iframe></center>
-        if(preg_match('/<center><iframe src=/', $html))
+        if(preg_match('/<center><iframe src=\'http:\/\/.+\' frameborder=\'0\' scrolling=\'no\'/', $html))
         {
             return true;
         }
