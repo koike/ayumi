fdd78ae44028afbb1ebd4bcb65b51588592ffd2d
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-04-14 11:55:43 +0900

    :up: Updated lib/EK/Magnitude.php

---
+++ lib/EK/Magnitude.php
@@ -0,0 +1,16 @@
+<?php
+
+class Magnitude
+{
+    public static function analyze(string $html) : bool
+    {
+        if(preg_match('/http:\/\/(?![a-z]+\.)[a-z0-9]{7,22}\.[a-z]{5,9}\.(?!com|org|net)[a-z]{2,7}\/(?![a-z]+$)[a-z0-9]{8,128}$/', $html))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
