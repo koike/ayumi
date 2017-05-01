ae3f82de51b7905a6e1d50d3f4c610b48f587efb
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-05-01 11:23:40 +0900

    :up: Updated lib/Campaign/Seamless.php

--- lib/Campaign/Seamless.php
+++ lib/Campaign/Seamless.php
@@ -4,56 +4,44 @@ class Seamless
 {
     public static function analyze(string $html) : bool
     {
-        // 改行を削除
-        $html = str_replace("\r", "", $html);
-        $html = str_replace("\n", "", $html);
+        preg_match_all('/<iframe.+>/', $html, $str);
+        for($i=0; $i<count($str); $i++)
+        {
+            // 全て小文字に変換
+            $iframe = strtolower($str[$i][0]);
+            
+            // <iframe width="0" scrolling="no" height="0" frameborder="0" src="" seamless="seamless">
+            $rate = 0;
+            
+            if(strpos($iframe, 'width=0') !== false)
+            {
+                $rate += 1;
+            }
+            if(strpos($iframe, 'height=0') !== false)
+            {
+                $rate += 1;
+            }
+            if(strpos($iframe, 'scrolling=no') !== false)
+            {
+                $rate += 1;
+            }
+            if(strpos($iframe, 'frameborder=0') !== false)
+            {
+                $rate += 1;
+            }
+            if(strpos($iframe, 'seamless=seamless') !== false)
+            {
+                $rate += 4;
+            }
 
-        // 空白系を削除
-        $html = str_replace("\t", "", $html);
-        $html = str_replace(' ', '', $html);
-
-        // クオート系を削除
-        $html = str_replace("'", '', $html);
-        $html = str_replace('"', '', $html);
-
-        // 全て小文字に変換
-        $html = strtolower($html);
-        
-        // <iframe width="0" scrolling="no" height="0" frameborder="0" src="" seamless="seamless">
-        $rate = 0;
-        
-        if(strpos($html, '<iframe') !== false)
-        {
-            $rate += 1;
-        }
-        if(strpos($html, 'width=0') !== false)
-        {
-            $rate += 1;
-        }
-        if(strpos($html, 'height=0') !== false)
-        {
-            $rate += 1;
-        }
-        if(strpos($html, 'scrolling=no') !== false)
-        {
-            $rate += 1;
-        }
-        if(strpos($html, 'frameborder=0') !== false)
-        {
-            $rate += 1;
-        }
-        if(strpos($html, 'seamless=seamless') !== false)
-        {
-            $rate += 4;
-        }
-
-        if($rate >= 6)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
+            if($rate >= 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
