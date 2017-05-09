a60af973fa2b150ef4ad1ffc6776653b028c7b62
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-05-09 11:25:01 +0900

    :up: Updated lib/Campaign/Unknown.php

--- lib/Campaign/Unknown.php
+++ lib/Campaign/Unknown.php
@@ -4,14 +4,20 @@ class Unknown
 {
     public static function analyze(string $html) : bool
     {
-        // function loadjscssfile(filename, filetype)
-        if(preg_match('/function loadjscssfile\(filename, filetype\)/', $html, $result))
+        $html = str_replace("\r", "", $html);
+        $html = str_replace("\n", "", $html);
+        $html = str_replace("\t", "", $html);
+        $html = str_replace(' ', '', $html);
+
+        if
+        (
+            strpos($html, 'function loadjscssfile(filename, filetype)') !== false &&
+            strpos($html, 'filetype=="css"') === false
+        )
         {
             return true;
         }
-        else
-        {
-            return false;
-        }
+
+        return false;
     }
 }
