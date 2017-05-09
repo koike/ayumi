5ea92dd24ab4227b1a93ff8fe05e20598cfb0a2e
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-05-09 11:01:56 +0900

    :up: Updated lib/Campaign/Unknown.php

--- lib/Campaign/Unknown.php
+++ lib/Campaign/Unknown.php
@@ -4,13 +4,8 @@ class Unknown
 {
     public static function analyze(string $html) : bool
     {
-        $html = str_replace("\r", "", $html);
-        $html = str_replace("\n", "", $html);
-        $html = str_replace("\t", "", $html);
-        $html = str_replace(' ', '', $html);
-
-        // <iframe style='hidden' src='EK URL' width='0' height='0'></iframe>
-        if(preg_match('/<iframestyle=\'hidden\'src=\'http:\/\/.+\'width=\'0\'height=\'0\'><\/iframe>/', $html, $result))
+        // function loadjscssfile(filename, filetype)
+        if(preg_match('/function loadjscssfile\(filename, filetype\)/', $html, $result))
         {
             return true;
         }
