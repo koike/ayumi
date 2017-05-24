84e34b0ff76eb45cbe6abed9cc1eef6b9ccb0555
koike <r_ko@live.jp>
2017-05-24 19:56:38 +0900

    :up: Updated lib/Campaign/EITest.php

--- lib/Campaign/EITest.php
+++ lib/Campaign/EITest.php
@@ -6,22 +6,10 @@ class EITest
     {
         $rate = 0;
         
-        // <body> </body>
-        if(preg_match('/<body> <\/body>/', $html))
-        {
-            $rate += 1;
-        }
-
         // iframeという文字列を持つ変数
         if(preg_match('/var ([a-zA-Z]{4,8}) = "iframe"/', $html))
         {
-            $rate += 1;
-        }
-
-        // http://小文字.大文字.大文字
-        if(preg_match('/https?:\/\/([a-z0-9\-_]+)\.([a-zA-Z0-9\-_]+)\.([a-zA-Z0-9]+)/', $html))
-        {
-            $rate += 0.5;
+            $rate += 2;
         }
 
         // frameBorder = "0"
@@ -42,7 +30,7 @@ class EITest
             $rate += 0.5;
         }
 
-        if($rate >= 2)
+        if($rate > 2)
         {
             return true;
         }
