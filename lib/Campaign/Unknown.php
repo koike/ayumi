6ac59ac1ad4d5bc61d29ecb24f564cc808248891
koike <r_ko@live.jp>
2017-06-14 14:52:16 +0900

    :up: Updated lib/Campaign/Unknown.php

--- lib/Campaign/Unknown.php
+++ lib/Campaign/Unknown.php
@@ -4,11 +4,11 @@ class Unknown
 {
     public static function analyze(string $html) : bool
     {
-        if(strpos($html, '/afu.php') !== false)
+        if(strpos($html, '/afu.php?zoneid=') !== false)
         {
             return true;
         }
-        if(strpos($html, '/popunder.php') !== false)
+        if(strpos($html, 'src="/popunder.php"') !== false)
         {
             return true;
         }
