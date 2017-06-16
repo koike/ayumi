c7db3795fff1c95fdeeea41fa9fdd17b43c820b7
koike <r_ko@live.jp>
2017-06-17 00:22:50 +0900

    :up: Updated HookAds.php

--- lib/Campaign/HookAds.php
+++ lib/Campaign/HookAds.php
@@ -4,6 +4,15 @@ class HookAds
 {
     public static function analyze(string $html) : bool
     {
+        if(strpos($html, 'abdulaporn') !== false)
+        {
+            return true;
+        }
+        if(strpos($html, 'likexhamster') !== false)
+        {
+            return true;
+        }
+
         // <center><iframe src='URL' frameborder='0' scrolling='no' width='xxx' height='xx'></iframe></center>
         if(preg_match('/<center><iframe src=\'http:\/\/.+\' frameborder=\'0\' scrolling=\'no\' width=\'[0-9]{2,4}\' height=\'[0-9]{2,4}\'><\/iframe><\/center>/', $html))
         {
