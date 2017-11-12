de0a49ebabbd456672374ce1fe37d0182436dfc7
koike <r_ko@live.jp>
2017-11-13 01:51:52 +0900

    :up: Updated lib/Campaign/Disdain.php

--- lib/Campaign/Disdain.php
+++ lib/Campaign/Disdain.php
@@ -7,9 +7,9 @@ class Disdain
         $html = str_replace("\r", "", $html);
         $html = str_replace("\n", "", $html);
         $html = str_replace("\t", "", $html);
-        $html = str_replace(' ', '', $html);
 
-        if(strpos($html, '</iframe><!DOCTYPE html>') !== false)
+        // if(strpos($html, '</iframe><!DOCTYPE html>') !== false)
+        if(preg_match('/<iframe src="http:\/\/.+" width="1" height="1" style="position:absolute;left:-1px;"><\/iframe>/', $html))
         {
             return true;
         }
